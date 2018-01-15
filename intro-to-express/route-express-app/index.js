e11bc40f6cdb08396c33d3aaf33000106eaffc21
Tim Austin <tim@neenjaw.com>
2018-01-15 08:20:02 +0000

    basic express routing done

---
+++ intro-to-express/route-express-app/index.js
@@ -0,0 +1,48 @@
+"use strict";
+
+let express = require('express'),
+    app = express();
+    
+let animalData = {
+    cow: {sound: "Moo"},
+    pig: {sound: "Oink"},
+    dog: {sound: "Woof Woof!"}
+};
+   
+// Root route
+app.get("/", (req, res) => {
+    res.send("Hi there, welcome to my assignment!");
+});
+
+// Pattern route
+app.get("/speak/:animal", (req, res) => {
+    let animal = req.params.animal;
+    
+    if (animal in animalData) {
+        res.send(`The ${animal} says '${animalData[animal].sound}'`);    
+    } else {
+        res.redirect(res.baseUrl + "/..");
+    }
+});
+
+// Pattern route
+app.get("/repeat/:string/:times", (req, res) => {
+    let response = "";
+    
+    for (let i = 0; i < req.params.times; i++) {
+        response += req.params.string + " ";
+    }
+    
+    response.trim();
+    
+    res.send(response);
+});
+   
+// Catch-all route 
+app.get("*", (req, res) => {
+   res.send("Sorry, page not found... What are you doing with your life.");
+});
+
+app.listen(process.env.PORT, process.env.IP, ()=>{
+    console.log("App starting");
+})
