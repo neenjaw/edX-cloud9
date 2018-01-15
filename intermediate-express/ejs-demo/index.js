c7c7a654aa0b2e3050f33e05e050eef7a640db85
neenjaw <tim@neenjaw.com>
2018-01-15 09:50:22 -0700

    ejs partials, express view engine, express static

--- intermediate-express/ejs-demo/index.js
+++ intermediate-express/ejs-demo/index.js
@@ -1,14 +1,27 @@
 const express = require('express'),
       app = express();
 
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
 app.get("/", (req, res) => {
-  res.render("home.ejs");
+  res.render("home");
 });
 
 app.get("/fellinlovewith/:thing", (req, res) => {
   let thing = req.params.thing;
 
-  res.render("fellinlove.ejs", {thing});
+  res.render("fellinlove", {thing});
+});
+
+app.get("/posts", (req, res) => {
+  let posts = [
+    {title: "1", author:"Susie"},
+    {title: "2", author:"George"},
+    {title: "3", author:"Barry"}
+  ];
+
+  res.render("posts", {posts});
 });
 
 app.listen(3000, "localhost", () => {
