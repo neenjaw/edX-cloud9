8b22458f3d09c2c9b066601089c93c040a67115d
Tim Austin <tim@neenjaw.com>
2018-02-23 17:17:24 -0700

    authorization added for comments, referential integrity added

--- yelp-camp/v9/routes/comment.js
+++ yelp-camp/v9/routes/comment.js
@@ -77,12 +77,109 @@ router.post('/', isLoggedIn, (req, res) => {
     });
 });
 
+//EDIT
+router.get('/:commentId/edit', isThisCommentOwner, (req, res) => {
+    res.render('comments/edit', {
+        pageName: 'campgrounds/comments/new', 
+        campground: {
+            id: req.params.id
+        },
+        comment: {
+            id: req.comment._id,
+            text: req.comment.text
+        }
+    });
+});
+
+//UPDATE
+router.put('/:commentId', isThisCommentOwner, (req, res) => {
+    const campgroundId = req.params.id;
+    const commentId = req.params.commentId;
+
+    const comment = req.body.comment;
+    comment.updated = Date.now();
+
+    const updateQuery = { $set: { text: comment.text, updated: comment.updated }};
+
+    Comment
+        .findByIdAndUpdate(commentId, updateQuery, (err, updatedComment) => {
+            if (err) {
+                console.log(err);
+                res.redirect('back');
+            } else {
+                console.log(updatedComment);
+                res.redirect(`/campgrounds/${campgroundId}`);
+            }
+        });
+});
+
+//DELETE
+router.delete('/:commentId', isThisCommentOwner, (req, res) => {
+    const campgroundId = req.params.id;
+    const commentId = req.params.commentId;
+
+    if (!commentId && campgroundId) {
+        res.redirect('back');
+    } else if (!commentId || !campgroundId) {
+        res.redirect('/campgrounds');
+    } else {
+        Comment
+            .findByIdAndRemove(commentId, (err) => {
+                if (err) {
+                    console.log(err);
+                    res.redirect('back');
+                } else {
+                    const query = { $pull: { comments: commentId }};
+
+                    Campground
+                        .findByIdAndUpdate(campgroundId, query, (err, campground) => {
+                            if (err) {
+                                console.log(err);
+                            } else {
+                                res.redirect(`/campgrounds/${campgroundId}`);
+                            }
+                        });
+                }
+            });
+    }
+});
+
 // isLoggedIn Middleware
 function isLoggedIn(req, res, next) {
     if ( req.isAuthenticated() ) {
         return next();
     }
     res.redirect('/login');
+}
+
+// check if this is the comment owner
+function isThisCommentOwner(req, res, next) {
+    // is user logged in?
+    if (!req.isAuthenticated()) {
+        //if not, redirect
+        res.redirect('back');
+    
+    } else {
+        //continue with edit
+        const id = req.params.commentId;
+    
+        Comment
+            .findById(id)
+            .exec((err, comment) => {
+                if (err) {
+                    //TODO: eventually replace with a redirect to the form with error
+                    res.redirect('back');
+                } else {
+                    //does the user own the campground?
+                    if (!comment.author.equals(req.user._id)) {
+                        res.redirect('back');
+                    } else {
+                        req.comment = comment;
+                        next();
+                    }
+                }
+            });
+    }
 }
 
 module.exports = router;
